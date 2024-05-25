545d9adeb2bb3b386d67d5b3f0dd76dfe47d4076
GioMench <giorgiaglm@gmail.com>
2024-05-25 17:41:26 +0200

    add type files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-
+use Database\Seeders\ProjectSeeder;
+use Database\Seeders\TypeSeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(
+            [
+                ProjectSeeder::class,
+                TypeSeeder::class
+            ]
+            );
     }
 }
---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = ['Fullstack', 'Front-end'];
+
+        foreach($types as $type){
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name, '-');
+            $newType->save();
+        }
+    }
+}
