18cc851910accced6619903853adc5489e575ba6
GioMench <giorgiaglm@gmail.com>
2024-05-27 12:40:04 +0200

    connet model type and project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['project_name', 'slug', 'description', 'preview_image', 'link_view', 'link_git', 'link_video', 'year_project' ];
+    protected $fillable = ['project_name', 'slug', 'description', 'preview_image', 'link_view', 'link_git', 'link_video', 'year_project', 'type_id' ];
+
+    /**
+     * Get the user that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
